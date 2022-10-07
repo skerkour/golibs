2438d94c2f91d8add981b8f937505d793fb4acae
Sylvain Kerkour <sylvain@kerkour.com>
2022-10-07 08:14:55 +0000

    Add autocertpg

---
+++ autocertpg/autocertpg.go
@@ -0,0 +1,82 @@
+package autocertpg
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/skerkour/libs/crypto"
+	"github.com/skerkour/libs/db"
+	"golang.org/x/crypto/acme/autocert"
+)
+
+type Cache struct {
+	key []byte
+	db  db.DB
+}
+
+type cert struct {
+	Key           string `db:"key"`
+	EncryptedData []byte `db:"encrypted_data"`
+}
+
+func NewCache(db db.DB, key []byte) *Cache {
+	return &Cache{
+		db:  db,
+		key: key,
+	}
+}
+
+func (cache *Cache) Get(ctx context.Context, key string) (data []byte, err error) {
+	var cert cert
+	query := "SELECT * FROM certs WHERE key = $1"
+
+	err = cache.db.Get(ctx, &cert, query, key)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			err = autocert.ErrCacheMiss
+		}
+		return
+	}
+
+	data, err = crypto.Decrypt(cache.key, cert.EncryptedData, []byte(cert.Key))
+	if err != nil {
+		err = fmt.Errorf("autocertpg: decrypting data: %w", err)
+		return
+	}
+
+	return
+}
+
+func (cache *Cache) Put(ctx context.Context, key string, data []byte) (err error) {
+	query := `
+	INSERT INTO certs (key, encrypted_data)
+		VALUES ($1, $2)
+		ON CONFLICT (key)
+		DO UPDATE SET encrypted_data = $2
+	`
+
+	encryptedData, err := crypto.Encrypt(cache.key, data, []byte(key))
+	if err != nil {
+		err = fmt.Errorf("autocertpg: encrypting data: %w", err)
+		return
+	}
+
+	_, err = cache.db.Exec(ctx, query, key, encryptedData)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (cache *Cache) Delete(ctx context.Context, key string) (err error) {
+	query := "DELETE FROM certs WHERE key = $1"
+
+	_, err = cache.db.Exec(ctx, query, key)
+	if err != nil {
+		return
+	}
+
+	return
+}
--- go.mod
+++ go.mod
@@ -27,6 +27,7 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/pgtype v1.12.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
