cfb5477fb42cdc7d04dc2c58939c702541b5aad7
Sylvain Kerkour <sylvain@kerkour.com>
2022-10-07 13:24:20 +0000

    languages: Add Get

--- languages/languages.go
+++ languages/languages.go
@@ -1,6 +1,32 @@
 package languages
 
-import _ "embed"
+import (
+	_ "embed"
+	"encoding/json"
+)
 
 //go:embed languages.json
 var Bytes []byte
+
+type Language struct {
+	Code       string `json:"code"`
+	Name       string `json:"name"`
+	NativeName string `json:"native_name"`
+}
+
+var langs map[string]Language
+
+func Get() (map[string]Language, error) {
+	var err error
+
+	if langs == nil {
+		langs = map[string]Language{}
+		err = json.Unmarshal(Bytes, &langs)
+		if err != nil {
+
+			return langs, err
+		}
+	}
+
+	return langs, nil
+}
