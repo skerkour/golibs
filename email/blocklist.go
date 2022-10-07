7ddedd7e623c4c5cb724f85a80cab2a77d06f551
Sylvain Kerkour <sylvain@kerkour.com>
2022-10-07 12:18:35 +0000

    email: Add IsInBlocklist

--- email/blocklist.go
+++ email/blocklist.go
@@ -1,6 +1,25 @@
 package email
 
-import _ "embed"
+import (
+	"bufio"
+	"bytes"
+	_ "embed"
+	"strings"
+)
 
 //go:embed domains_blocklist.txt
 var BlocklistBytes []byte
+
+var blocklist map[string]bool
+
+func IsInBlocklist(email string) bool {
+	if blocklist == nil {
+		mailBlocklistFileReader := bytes.NewReader(BlocklistBytes)
+		mailBlocklistScanner := bufio.NewScanner(mailBlocklistFileReader)
+		for mailBlocklistScanner.Scan() {
+			blocklist[strings.TrimSpace(mailBlocklistScanner.Text())] = true
+		}
+	}
+
+	return blocklist[email]
+}
